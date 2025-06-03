c6db58f6a07408d1baab47b00e41d44d50166cce
Diego0119 <diegosanhuezaaguilar@gmail.com>
2025-06-03 16:22:34 -0300

    añadiendo validacion en el dto

--- src/auth/Dto/create-user.dto.ts
+++ src/auth/Dto/create-user.dto.ts
@@ -1,14 +1,14 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-    @IsEmail()
+    @IsEmail({}, { message: ' Se debe ingresar un correo valido.\n' })
     readonly email: string;
 
     @IsString()
-    @MinLength(4)
+    @MinLength(4, { message: 'La contraseña debe tener al menos 4 caracteres.\n' })
     readonly password: string;
 
     @IsString()
-    @MinLength(4)
+    @MinLength(4, { message: 'La confirmación de la contraseña debe tener al menos 4 caracteres.\n' })
     readonly confirmedPassword: string;
 }

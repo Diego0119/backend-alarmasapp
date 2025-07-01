0dce4886ebf834fe0ba924a2b671154cc77afb75
ChrissNMS <christofer.guti555@gmail.com>
2025-07-01 00:11:41 -0400

    Update pagos.module.ts

--- src/pagos/pagos.module.ts
+++ src/pagos/pagos.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PagosService } from './pagos.service';
 import { PagosController } from './pagos.controller';
-import { Pagos } from './entities/pagos.entity';
+import { Pago } from './entities/pagos.entity'; 
 import { Alarmas } from '../alarmas/alarm.entity';
 import { Servicio } from '../servicios/servicios.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pagos, Alarmas, Servicio])],
+  imports: [TypeOrmModule.forFeature([Pago, Alarmas, Servicio])], 
   providers: [PagosService],
   controllers: [PagosController],
 })

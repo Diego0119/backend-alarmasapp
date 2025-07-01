16ca1b121955b2572182e84594a270bed068f002
ChrissNMS <christofer.guti555@gmail.com>
2025-07-01 00:11:17 -0400

    Update create-user.dto.ts

--- src/auth/Dto/create-user.dto.ts
+++ src/auth/Dto/create-user.dto.ts
@@ -1,27 +1,35 @@
 import {
   IsEmail,
+  IsOptional,
   IsString,
   MinLength,
 } from 'class-validator';
-import { Expose } from 'class-transformer'; 
+import { Expose } from 'class-transformer';
 
 export class CreateUserDto {
+  @IsOptional()
+  @IsString()
+  @Expose()
+  readonly nombre_usuario?: string;
+
   @IsEmail({}, { message: 'Se debe ingresar un correo válido.\n' })
   @Expose()
-  readonly email: string;
+  readonly correo: string;
 
+  @IsOptional()
   @IsString()
   @MinLength(4, {
     message: 'La contraseña debe tener al menos 4 caracteres.\n',
   })
   @Expose()
-  readonly password: string;
+  readonly password?: string;
 
+  @IsOptional()
   @IsString()
   @MinLength(4, {
     message:
       'La confirmación de la contraseña debe tener al menos 4 caracteres.\n',
   })
   @Expose()
-  readonly confirmedPassword: string;
+  readonly confirmedPassword?: string;
 }

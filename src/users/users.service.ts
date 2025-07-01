0a1d11a46fa2e1df779ac40f94ab98c7ec1449b7
ChrissNMS <christofer.guti555@gmail.com>
2025-07-01 00:14:04 -0400

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Usuario } from './user.entity';
@@ -13,17 +13,41 @@ export class UsersService {
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<Usuario> {
-    const salt = await bcrypt.genSalt(); 
+    if (!createUserDto.password) {
+      throw new BadRequestException('La contraseña es requerida');
+    }
+
+    const salt = await bcrypt.genSalt();
+
+    if (!createUserDto.password) {
+      throw new BadRequestException('La contraseña es requerida');
+    }
+
     const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
 
     const user = this.usersRepository.create({
-      correo: createUserDto.email,
+      correo: createUserDto.correo,
       contrasena: hashedPassword,
     });
+
     return this.usersRepository.save(user);
   }
 
+  async findByCorreo(correo: string): Promise<Usuario | null> {
+    return this.usersRepository.findOne({ where: { correo } });
+  }
+
   async findByEmail(email: string): Promise<Usuario | null> {
-    return this.usersRepository.findOne({ where: { correo: email } });
+    return this.findByCorreo(email);
+  }
+
+  async createWithGoogle(nombre: string, correo: string): Promise<Usuario> {
+    const user = this.usersRepository.create({
+      nombre,
+      correo,
+      contrasena: null,
+    });
+
+    return this.usersRepository.save(user);
   }
 }
